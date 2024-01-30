1afb913f1fd455501a087dcbf8969ff8fb2749ad
Saurav-VIT <156888667+Saurav-VIT@users.noreply.github.com>
2024-01-30 07:24:05 +0000

    Add one to many association

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_01_30_042917) do
+ActiveRecord::Schema.define(version: 2024_01_30_061150) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", precision: 6
     t.datetime "updated_at", precision: 6
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
