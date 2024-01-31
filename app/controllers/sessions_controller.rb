d7ce51facc86da4c663a77d82fcf815343cd4f2e
Saurav-VIT <156888667+Saurav-VIT@users.noreply.github.com>
2024-01-31 12:21:46 +0000

    up to sl.154 of section 6

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new; end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:notice] = "Welcome to Nebula Notes #{user.username}, you have successfully logged in!"
+      redirect_to user
+    else
+      flash.now[:alert] = 'Invalid email/password combination'
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = 'You have successfully logged out!'
+    redirect_to root_path
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,13 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @articles = @user.articles
+    @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
+
+  def index
+    @users = User.paginate(page: params[:page], per_page: 5)
+  end
+
   def new
     @user = User.new
   end
@@ -14,8 +19,8 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      flash[:notice] = "Your account has been updated."
-      redirect_to articles_path
+      flash[:notice] = 'Your account has been updated.'
+      redirect_to @user
     else
       render 'edit', status: :unprocessable_entity
     end
