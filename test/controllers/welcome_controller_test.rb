a424800f19012c56a40c7ed0a97def37a006d21f
Saurav-VIT <156888667+Saurav-VIT@users.noreply.github.com>
2024-02-02 13:52:12 +0000

    Upto section 6 , SL.198

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -1,8 +1,17 @@
+# require "test_helper"
+
+# class WelcomeControllerTest < ActionDispatch::IntegrationTest
+#   test "should get index" do
+#     get welcome_index_url
+#     assert_response :success
+#   end
+# end
+
 require "test_helper"
 
 class WelcomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get welcome_index_url
+  test "should get home" do
+    get root_url
     assert_response :success
   end
 end
